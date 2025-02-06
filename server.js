f09172e5e03a9b2223a9764608a802e3cf3ae8b4
tzuchen <tzuchen@tzuchendeMacBook-Pro.local>
2025-02-07 01:11:01 +0800

    建立server接收line使用者訊息

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { handleMessage } = require('./lineBot');
+
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    res.sendStatus(200);
+});
+
+app.post('/', (req, res) => {
+    const events = req.body.events;
+    events.forEach(event => {
+        if (event.type === 'message' && event.message.type === 'text') {
+            handleMessage(event);
+        }
+    });
+    res.sendStatus(200);
+});
+
+// 啟動伺服器
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`✅ 伺服器運行中: http://localhost:${PORT}`);
+});
