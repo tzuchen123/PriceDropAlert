cab281e283570505a4bd13a27b64212d2b785cc3
tzuchen <tzuchen@tzuchendeMacBook-Pro.local>
2025-02-15 03:34:14 +0800

    先改回0.0.0.0

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.post('/', (req, res) => {
 
 // 啟動伺服器
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '::', () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ 伺服器運行中: http://[::]:${PORT}`);
 });
+
